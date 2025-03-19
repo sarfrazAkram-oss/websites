4204a2261bf7b7c7791a5c124c8f5fc86ef7e2fd
sarfrazAkram-oss <miansarfaraz206905@gmail.com>
2025-03-19 12:30:36 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+ /***  Data types  ***/
+string, number, boolean, object,  undefined
+var x;
+x = "a";
+x = 1.5;
+x = true;
+x = ["1", "name", "true"];
+x = {"name":"abc", "id":1};
+console.log(typeof(x));
